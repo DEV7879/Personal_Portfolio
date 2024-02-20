3444ca0d22a034eb81ed66a6e510b1bae3f44dc8
DEV7879 <devendrabishnoi881@gmail.com>
2024-02-20 20:49:59 +0530

    Updates

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-3b38c93e54f9c9c13ff1.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkDevPortfolioWebsite=self.webpackChunkDevPortfolioWebsite||[]).push([[306],{1930:function(e,t,n){var u=n(5318);t.__esModule=!0,t.default=void 0;var o=u(n(5354)),r=u(n(7294)),l=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component),f=l;t.default=f}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-3b38c93e54f9c9c13ff1.js.map
